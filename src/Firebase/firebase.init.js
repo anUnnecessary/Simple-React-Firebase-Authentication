b4067dc558d2e2d260c863355c813a0377d65b2c
anUnnecessary <raihanshamil33@gmail.com>
2023-04-13 00:21:45 +0600

    first commit

---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA9Tj0Y3W36FAgHpIQFAFNOscwHcWZrs04",
+  authDomain: "simple-firebase-61656.firebaseapp.com",
+  projectId: "simple-firebase-61656",
+  storageBucket: "simple-firebase-61656.appspot.com",
+  messagingSenderId: "802132815308",
+  appId: "1:802132815308:web:6b19b6a309f1d1fdcc0169"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/component/Login/Login.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth'
+import app from '../../Firebase/firebase.init';
+
+const Login = () => {
+    const auth = getAuth(app);
+    const provider = new GoogleAuthProvider();
+
+    const handleGoogleSignIn = () =>{
+        signInWithPopup(auth, provider)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(error => {
+            console.log('404 not found', error.message);
+        })
+    }
+
+    return (
+        <div>
+          <button onClick={handleGoogleSignIn}>Google Login</button>
+        </div>
+    );
+};
+
+export default Login;
